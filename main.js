9059edd766d681f4a23607ee918b602f62ddef03
Connor J Thompson <ct770a@gmail.com>
2024-10-03 22:02:06 +1000

    its now successfully sending out logon requests and recieving and processing the container packets

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ import Long from "/DEPENDENCIES/Long.js";
 // NOTE: uh
 var CMsgHeader_Message = null;
 var CMsgLogon_Message = null;
+var CMsgMulti_Message = null;
 protobuf.load("PROTOGEN/clientserver_login.proto", function(err, root) {
     if (err) throw err;
     CMsgHeader_Message = root.lookupType("steamproto.CMsgProtoBufHeader");
     CMsgLogon_Message = root.lookupType("steamproto.CMsgClientLogon");
+    CMsgMulti_Message = root.lookupType("steamproto.CMsgMulti");
+
+    init_steam_connection();
 });
 
 
@@ -17,12 +21,12 @@ const ClientHello_packet = [ 0x4D, 0x26, 0x00, 0x80,  0x00, 0x00, 0x00, 0x00,  0
 const ClientHello_view = new Uint8Array(new ArrayBuffer(ClientHello_packet.length));
 ClientHello_view.set(ClientHello_packet);
 function Steam_SendHello(){
-    ws.send(ClientHello_view)
+    WS.send(ClientHello_view)
 }
 
 function Steam_SendLogon(){
     let header = {
-        clientSessionid: 0, // NOTE: this gets encoded in steamkit C#, but not this???
+        clientSessionid: 0,
         steamid: new Long(0x00000000, 0x01100001) // 0x0110000100000000
     };
     let logon = {
@@ -42,7 +46,8 @@ function Steam_SendLogon(){
         //machine_id: HardwareUtils.GetMachineID( Client.Configuration.MachineInfoProvider ) // pretty sure we cant complete this via browser APIs (NOTE: 0x9b bytes is what we'd usually get from this)
     };
     let serialized = SerializePacket([0x8A,0x15,0x0,0x80], header, CMsgHeader_Message, logon, CMsgLogon_Message);
-    ws.send(serialized)
+    WS.send(serialized)
+    console.log("logon sent!!!")
 }
 function Steam_SendHeartbeat(){
 
@@ -52,7 +57,7 @@ function Steam_SendWorkshopQuery(){
 }
 
 function Steam_RecieveLogon(){
-    
+
 }
 
 function SerializePacket(msg_sig, header, headerproto, body, bodyproto){
@@ -74,26 +79,126 @@ function proto_serialize(payload, proto){
     if (errMsg) throw Error("proto issue: "+errMsg);
 
     let result = proto.encode(proto.create(payload)).finish();
-    console.log(result);
     return result;
 }
 function int_to_4byte(num){ // encodes to little endian!!!
     return [(num & 0x000000ff), (num & 0x0000ff00) >> 8, (num & 0x00ff0000) >> 16, (num & 0xff000000) >> 24];
 }
-
-
-
-function DeserializePacket(){
-
-    var message = AwesomeMessage.decode(buffer);
-    // Maybe convert the message back to a plain object
-    var object = AwesomeMessage.toObject(message, {
-        longs: String,
-        enums: String,
-        bytes: String,
-    });
+function typedArrayToBuffer(array) {
+    return array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset)
+}
+async function concatUint8Arrays(uint8arrays) {
+    const blob = new Blob(uint8arrays);
+    const buffer = await blob.arrayBuffer();
+    return new Uint8Array(buffer);
 }
 
+
+async function DeserializePacket(buffer){
+    if (buffer.length < 4) throw "recieved packet too small to contain any information!!";
+    
+    let dataView = new DataView(typedArrayToBuffer(buffer));
+
+    // Read the first Uint32 value (4 bytes)
+    let message_type = dataView.getUint32(0, true);
+    let is_proto = message_type >>> 31;
+    message_type &= 0x7fffffff;
+    
+
+
+    // custom header ???
+    if (message_type == 1303 || message_type == 1304 || message_type == 1305){
+        //EMsg.ChannelEncryptRequest EMsg.ChannelEncryptResponse EMsg.ChannelEncryptResult:
+        console.log("recieved packet that we aren't supposed to read??");
+        return null;
+    } 
+
+    let read_position = 4;
+    let HeaderSize = 0;
+    let header_object = null;
+
+    // if proto header 
+    if (is_proto == 1){
+        read_position += 4;
+        HeaderSize = dataView.getUint32(4, true); 
+        // Maybe convert the message back to a plain object
+        header_object = proto_deserialize(buffer.subarray( 8, 8 + HeaderSize), CMsgHeader_Message);
+    } else { // otherwise struct header (NOTE: probably not used but steamkit has this so) // WARNING: names have not been matched up, so consider this NOT-IN-USE
+        throw "packet had non-proto header!! currently unsupported!!!"
+        // read_position += 1;
+        // HeaderSize = dataView.getUint8(4);
+        // header_object = {
+        //     HeaderVersion: dataView.getUint16(5, true),
+        //     TargetJobID:   dataView.getBigUint64(7, true),
+        //     SourceJobID:   dataView.getBigUint64(15, true),
+        //     HeaderCanary:  dataView.getUint8(23),
+        //     steamid:       dataView.getBigUint64(24, true),
+        //     SessionID:     dataView.getInt32(32, true)
+        // };
+    }
+    read_position += HeaderSize;
+
+    console.log("recieved packet, type: " + enum_dict[message_type]+ ", proto: " + is_proto);
+
+
+    // deserialize body??
+    if        (message_type == 1){ // Multi
+        let multi_obj = proto_deserialize(buffer.subarray(read_position), CMsgMulti_Message);
+        let multi_buf = null;
+
+        if (multi_obj.sizeUnzipped != undefined && multi_obj.sizeUnzipped > 0){
+            // Convert the bytes to a stream.
+            const stream = new Blob([multi_obj.messageBody]).stream();
+            const decompressedStream = stream.pipeThrough(new DecompressionStream("gzip"));
+            
+            // Read all the bytes from this stream.
+            const chunks = [];
+            for await (const chunk of decompressedStream) { chunks.push(chunk); }
+            multi_buf = await concatUint8Arrays(chunks);
+        } else {
+            multi_buf = multi_obj.messageBody;
+        }
+        
+        read_position = 0;
+        while (read_position+4 <= multi_buf.length){
+            // read bundle length
+            let chunk_length = multi_buf[read_position] | (multi_buf[read_position+1] << 8) | (multi_buf[read_position+2] << 16) | (multi_buf[read_position+3] << 24);
+            read_position += 4;
+            if (read_position+chunk_length > multi_buf.length)
+                throw "trying to read multi chunk out of bounds!!";
+
+            await DeserializePacket(multi_buf.subarray(read_position, read_position + chunk_length));
+            read_position += chunk_length;
+        }
+        if (read_position != multi_buf.length){
+            console.log("leftover bytes from reading multi packet: " + read_position +", "+ multi_buf.length)
+        }
+
+    } else if (message_type == 751){ // ClientLogOnResponse
+
+    } else if (message_type == 757){ // ClientLoggedOff 
+
+    } else if (message_type == 5500){ // ClientServerUnavailable 
+
+    } else if (message_type == 783){ // ClientCMList
+
+    } else if (message_type == 850){ // ClientSessionToken
+
+    } else if (message_type == 146){ // ServiceMethod
+
+    } else if (message_type == 147){ // ServiceMethodResponse
+
+    }
+
+    return {type: "", header: "", body: ""}
+}
+function proto_deserialize(buffer, proto){
+    return proto.toObject(proto.decode(buffer), {
+        longs: String,
+        enums: String,
+        //bytes: String, // default enocdes them into uint8arrays, why would we want anything other than that ??????????
+    });
+}
 
 
 // NOTE: SOURCED FROM STEAMKIT2 (will add relevant links later)
@@ -116,34 +221,54 @@ function DeserializePacket(){
 //THING();
 
 
+var WS = null;
+function init_steam_connection(){
+    let custom_input_server = "cmp1-vie1.steamserver.net:27018";
+
+    let WSS_SERVER = "wss://"+custom_input_server+"/cmsocket/";
+    WS = new WebSocket(WSS_SERVER)
+    WS.onopen = () => {
+        console.log('WS opened')
+        Steam_SendHello();
+        Steam_SendLogon();
+    }
+
+    WS.onmessage = (message) => {
+        console.log("packet recieved!!")
+
+        if (message.data instanceof ArrayBuffer){
+            DeserializePacket(messagmessage.data);
+
+        } else if (message.data instanceof Blob){
+            var fileReader = new FileReader();
+            fileReader.onload = function(event) {
+                let arrayBufferNew = event.target.result;
+                let uint8ArrayNew  = new Uint8Array(arrayBufferNew);
+
+                DeserializePacket(uint8ArrayNew);
+            };
+            fileReader.readAsArrayBuffer(message.data);
+
+        } else if (typeof message.data === "string"){
+            console.log("packet recieved that was just a string???")
+            console.log(message.data)
+        }
+    }
+
+
+
+    WS.onerror = (event) =>{
+        console.log('WS error')
+    }
+    WS.onclose = (event) =>{
+        console.log('WS closed')
+    }
+}
 
 
 
 
 
-
-// let custom_input_server = "cmp1-vie1.steamserver.net:27018";
-// let WSS_SERVER = "wss://"+custom_input_server+"/cmsocket/";
-// // const ws = new WebSocket(WSS_SERVER)
-// ws.onopen = () => {
-//     console.log('ws opened')
-//     ws.send(ClientHello_view) // HELLO PACKET
-//     ws.send() // LOGON PACKET
-// }
-
-// ws.onmessage = (message) => {
-//     console.log(`message received`, message.data)
-//     // process message for type
-// }
-
-
-
-// ws.onerror = (event) =>{
-//     console.log('ws error')
-// }
-// ws.onclose = (event) =>{
-//     console.log('ws closed')
-// }
 
 
 
